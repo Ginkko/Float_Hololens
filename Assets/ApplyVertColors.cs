e08fbc52bb266e93a8fa6d51b481d4cd6dfdb507
Alec Arme <Alec.Arme@Heliosinteractive.com>
2016-06-11 17:07:45 -0700

    working implementation

---
+++ Assets/ApplyVertColors.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using ProceduralToolkit;
+
+public class ApplyVertColors : MonoBehaviour {
+
+    private Mesh myMesh;
+	// Use this for initialization
+	void Start () {
+
+        myMesh = GetComponent<MeshFilter>().sharedMesh;
+        
+       
+        Color[] colors = new Color[myMesh.vertexCount];
+        //Debug.Log(colors.Length);
+      
+
+        for (int i = 0; i < myMesh.vertexCount; i++)
+        {
+            colors[i] = RandomE.colorHSV;
+        }
+
+        myMesh.colors = colors;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	    
+
+	}
+
+
+
+}
---
+++ Assets/UpdateMeshPos.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using ProceduralToolkit;
+
+public class UpdateMeshPos : MonoBehaviour {
+
+    private MeshFilter myMeshFilter;
+
+    private MeshDraft draft;
+    private MeshDraft draftOrig;
+
+	// Use this for initialization
+	void Start () {
+        myMeshFilter = GetComponent<MeshFilter>();
+
+        draftOrig = new MeshDraft(myMeshFilter.sharedMesh);
+        draft = draftOrig;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        MoveVerts(transform.position);
+	}
+
+    void MoveVerts(Vector3 position)
+    {
+        draft = new MeshDraft(draft.ToMesh());
+
+        //draft.Move(new Vector3(0, 0, val));
+
+       
+
+        int vertexCount = draft.vertices.Count;
+
+        //Vector3[] verts = new Vector3[vertexCount];
+
+        List<Vector3> verts = new List<Vector3>();
+
+        for (int i = 0; i < vertexCount; i++)
+        {
+            Vector3 vertex = draftOrig.vertices[i];
+            vertex += position;
+            verts.Add(vertex);
+        }
+
+        draft.vertices = verts;
+        myMeshFilter.sharedMesh = draft.ToMesh();
+
+        //myMeshFilter.sharedMesh.SetVertices(verts);
+        //myMeshFilter.sharedMesh.UploadMeshData(false);
+    }
+
+}
